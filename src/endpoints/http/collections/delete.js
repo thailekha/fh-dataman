ef115f3220ba3dd2f054d28724f4aed1d3f632fe
Thai Le <thale@redhat.com>
2017-02-03 13:59:54 +0000

    introduce Left and Right

--- src/endpoints/http/collections/delete.js
+++ src/endpoints/http/collections/delete.js
@@ -1,69 +1,164 @@
-const _ = require('ramda');
+const _r = require('ramda');
 
 //===================
 // Reusable (start)
 //===================
 
-/* For debugging
-const log = _.curry(function (id,x){
+//For debugging
+const log = _r.curry(function (id,x){
   console.log('=====');
   console.log('logger ' + id);
   console.dir(x);
   console.log('=====');
   return x;
 });
-*/
 
 //=====================================
 //Functor and Functor utilities (start)
 //=====================================
 
-const Maybe = function(x) {
+/**
+ * Either Left functor
+ * @param {any} x Anything to be held in the functor
+ */
+const Left = function(x) {
   this.__value = x;
 };
 
-Maybe.of = function(x) {
-  return new Maybe(x);
+/**
+ * Construct a Left functor
+ * @constructor
+ * @param {any} x Anything to be held in the functor
+ * @return {Left} Left functor
+ */
+Left.of = function(x) {
+  return new Left(x);
 };
 
-Maybe.prototype.isNothing = function() {
-  return this.__value === undefined || this.__value === null;
+/**
+ * Identity
+ * @return {Left} Left functor
+ */
+Left.prototype.map = function() {
+  return this;
 };
 
-Maybe.prototype.map = function(f) {
-  return this.isNothing() ? Maybe.of(null) : Maybe.of(f(this.__value));
+/**
+ * Identity
+ * @return {Left} Left functor
+ */
+Left.prototype.join = function() {
+  return this;
 };
 
-Maybe.prototype.join = function() {
-  return this.isNothing() ? Maybe.of(null) : this.__value;
+/**
+ * Identity
+ * @return {Left} Left functor
+ */
+Left.prototype.chain = function() {
+  return this;
 };
 
-Maybe.prototype.chain = function(f) {
+/**
+ * Either Right functor
+ * @param {any} x Anything to be held in the functor
+ */
+const Right = function(x) {
+  this.__value = x;
+};
+
+/**
+ * Construct a Right functor
+ * @constructor
+ * @param {any} x Anything to be held in the functor
+ * @return {Right} Right functor
+ */
+Right.of = function(x) {
+  return new Right(x);
+};
+
+/**
+ * Apply a function to the value within the functor
+ * @param {function} f Function to be applied
+ * @return {Right} Right functor
+ */
+Right.prototype.map = function(f) {
+  return Right.of(f(this.__value));
+};
+
+/**
+ * Ressolve the nested functor
+ * @return {Right} Right functor
+ */
+Right.prototype.join = function() {
+  return this.__value;
+};
+
+/**
+ * Apply a function to the value within the functor,
+ * the function also returns a functor which results in a nested functor.
+ * So `join` is done to ressolve the nesting.
+ * @param {function} f Function to be applied
+ * @return {Right} Right functor
+ */
+Right.prototype.chain = function(f) {
   this.map(f).join();
 };
 
 /**
- * Pull out the value from the functor
+ * Retrieve the actual value from the functor and apply a function on it
+ * @param {any} err Anything to be returned if the functor is a Left
+ * @param {function} f The function to be applied on the actual value
+ * @param {functor} functor The functor that holds the desired value
+ * @returns {any}
  */
-//  maybe :: b -> (a -> b) -> Maybe a -> b
-const maybe = _.curry(function(err, f, functor) {
-  return functor.isNothing() ? err : f(functor.__value);
+const either = _r.curry(function(err,f,functor) {
+  switch (functor.constructor) {
+  case Left: return (err === undefined || err === null) ? functor : err; //???
+  case Right: return f(functor.__value);
+  }
+  //may need to throw exception here for invalid functor
 });
 
 /**
- * Apply a function on a functor
+ * (Functor Map - point-free style) Apply a function on a functor
+ * @param {function} f Function to be applied
+ * @param {functor} functor Functor which the function will be applied on
+ * @return {functor}
  */
-const map = _.curry((f, functor) => functor.map(f));
+const _m = _r.curry(function(f, functor) {
+  return functor.map(f);
+});
 
 /**
- * Apply a function that returns a functor on a functor
+ * (Functor Chain - point-free style)
+ * Apply a function that returns a functor on a functor and ressolve the nesting (map + join)
+ * @param {function} f Function to be applied
+ * @param {functor} functor Functor which the function will be applied on
+ * @return {functor}
  */
-const chain = _.curry(function(f, functor) {
+const _c = _r.curry(function(f, functor) {
   return functor.map(f).join();
 });
 
-const conditionalIdentity = _.curry(function(predicate, item) {
-  return predicate(item) ? Maybe.of(item) : Maybe.of(null);
+/**
+ * Access a property (attribute) of an object
+ * @param {String} prop The property
+ * @param {object} obj The object
+ * @returns {functor}
+ */
+const safeProp = _r.curry(function(prop, obj) {
+  return obj.hasOwnProperty(prop) ? Right.of(obj[prop]) : Left.of("Object does not have " + prop + " property");
+});
+
+/**
+ * Similar to identity function but the result is put in a functor and is either the item or an error message
+ * @param {function} predicate Function the returns a boolean
+ * @param {any} item
+ * @return {functor}
+ */
+const conditionalIdentity = _r.curry(function(predicate, item) {
+  return predicate(item) ? Right.of(item) : Left.of("Item " + item + " does not satisfy predicate");
 });
 
 //=====================================
@@ -74,36 +169,46 @@ const conditionalIdentity = _.curry(function(predicate, item) {
 //Other utilities (start)
 //=======================
 
+/**
+ * Return whatever passed in (like identity in maths)
+ * @param {any} x
+ * @return {any}
+ */
 const identity = x => x;
 
 /**
  * Greater than or equal to
+ * @param {number} y
+ * @param {number} x
+ * @return {boolean}
  */
-const gte = _.curry(function(y, x) {
-  return x >= y;
-});
+const gte = _r.curry((y, x) => x >= y);
 
 /**
- *  Get index of an item in an array
+ * Get index of an item in an array
+ * @param {[]} xs
+ * @param {any} x
+ * @return {number}
  */
-const indexIn = _.curry((xs, x) => xs.indexOf(x));
+const indexIn = _r.curry((xs, x) => xs.indexOf(x));
 
 /**
  * Check if an item is an an array
+ * @param {[]} xs
+ * @param {any} (omitted)
+ * @param {boolean}
  */
-const isInArray = xs => _.compose(gte(0), indexIn(xs));
+const isInArray = xs => _r.compose(gte(0), indexIn(xs));
 
 /**
- * Push an item onto an array and return the array
+ * Push an item to an array and return the array
+ * @param {[]} xs The array
+ * @param {object} x The item to be pushed on the array
+ * @returns {[]} The array with the new item added
  */
-const pushAndReturn = _.curry(function(xs, x) {
+const pushAndReturn = _r.curry(function(xs, x) {
   xs.push(x);
   return xs;
-});
-
-//safeProp :: string -> a -> b
-const safeProp = _.curry(function(prop, obj) {
-  return obj.hasOwnProperty(prop) ? Maybe.of(obj[prop]) : Maybe.of(null);
 });
 
 //=======================
@@ -123,28 +228,34 @@ const safeProp = _.curry(function(prop, obj) {
  * @param {String[]} collections The array of collection name(s) to be deleted.
  * @returns Promise
  */
-const deleteCollection = _.curry(function(appname, logger, db, collection) {
+const deleteCollection = _r.curry(function(appname, logger, db, collection) {
   logger.debug({appname}, 'deleting collection');
+
   const handlerDeleted = () => ({name: collection});
+
   return db.dropCollection(collection).then(handlerDeleted); //send db through the pipe
 });
 
 export default function deleteCollections(appname, logger, db, reqCollections) {
-  //const collectionItemPredicate = _.compose(maybe(false,identity), map(isInArray(reqCollections)), safeProp('name'));
-  const filterCollectionItem = conditionalIdentity(isInArray(reqCollections));
-
-  //for reducing
-  // :: [a] -> a -> [a]
-  const filterAndCollectDeletePromises = (accPromises, item) => _.compose(
-    maybe(accPromises,identity),
-      map(_.compose(pushAndReturn(accPromises),deleteCollection(appname, logger, db))), 
-        chain(filterCollectionItem),
-          safeProp('name')
-  )(item); //apply item to the composed function
-
   const promiseAll = xs => Promise.all(xs);
 
-  const handleSucessfulListCollections = collections => _.compose(promiseAll,_.reduce(filterAndCollectDeletePromises,[]))(collections); //apply collection to the composed function
+  const collectionArrays = db => db.listCollections().toArray();
 
-  return db.listCollections().toArray().then(handleSucessfulListCollections);
+  const filterCollectionItem = conditionalIdentity(isInArray(reqCollections));
+
+  const filterAndCollectDeletePromises = (accPromises, item) => _r.compose(
+    either(accPromises,identity), //temporary solution, discouraged
+    _m(_r.compose(pushAndReturn(accPromises),deleteCollection(appname, logger, db))), 
+    _c(filterCollectionItem),
+    safeProp('nam')
+  )(item); //apply item to the composed function
+
+  const handleSucessfulListCollections = collections => _r.compose(log(1),promiseAll,log(0),_r.reduce(filterAndCollectDeletePromises,[]))(collections); //apply collection to the composed function
+
+  var result = .then(handleSucessfulListCollections);
+
+  console.log(result);
+
+  return result;
+  //return db.listCollections().toArray().then(handleSucessfulListCollections);
 }
